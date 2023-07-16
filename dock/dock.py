05a4257c6f16658dbcadc13dadc8a334d1228ecc
iambstha <iambstha@gmail.com>
2023-07-16 11:38:13 +0545

    check title, availability of elements and clickablility of button features added

--- dock/dock.py
+++ dock/dock.py
@@ -14,10 +14,112 @@ driver = webdriver.Chrome(options=options)
 driver.maximize_window
 
 class Driver:
+    # Initialized with driver
     def __init__(self, driver):
         self.driver = driver
 
-    # Rerun in dock
+    # Gets the title of a webpage
+    def getTitle(self):
+        print(driver.title)
+    
+    # Only checks button & input of type submit
+    def checkClick(self,element):
+        cur_url = driver.current_url
+        match element:
+            case "button":
+                try:
+                    elem = driver.find_element(By.XPATH,  "//button[@type='submit']")
+                    elem.click()
+                    new_url = driver.current_url
+                    if cur_url != new_url:
+                        print("The button is clickable.")
+                    else:
+                        print("The button is not clickable.")
+                except NoSuchElementException as err:
+                    print("Exception: "+ err.msg)
+                except err:
+                    print("Error: " + err.msg)
+            case "input":
+                try:
+                    elem = driver.find_element(By.XPATH, "//input[@type='submit']")
+                    elem.click()
+                    new_url = driver.current_url
+                    if cur_url != new_url:
+                        print("The button is clickable.")
+                    else:
+                        print("The button is not clickable.")
+                except NoSuchElementException as err:
+                    print("Exception: "+ err.msg)
+                except err:
+                    print("Error: " + err.msg)
+            case _:
+                print("None")
+
+    # Checks for the availability of an element in a page
+    def checkElement(self,elem,i):
+        match elem:
+            case "ID":
+                try:
+                    e = driver.find_element(By.ID, i)
+                    print("The element with ID '" + i + "' is available.")
+                except NoSuchElementException as err:
+                    print("Exception: " + err.msg)
+            
+            case "NAME":
+                try:
+                    e = driver.find_element(By.NAME, i)
+                    print("The element with name '" + i + "' is available.")
+                except NoSuchElementException as err:
+                    print("Exception: " + err.msg)
+            
+            case "CLASS_NAME":
+                try:
+                    e = driver.find_element(By.CLASS_NAME, i)
+                    print("The element with class name '" + i + "' is available.")
+                except NoSuchElementException as err:
+                    print("Exception: " + err.msg)
+            
+            case "TAG_NAME":
+                try:
+                    e = driver.find_element(By.TAG_NAME, i)
+                    print("The element with tag name '" + i + "' is available.")
+                except NoSuchElementException as err:
+                    print("Exception: " + err.msg)
+            
+            case "LINK_TEXT":
+                try:
+                    e = driver.find_element(By.LINK_TEXT, i)
+                    print("The element with link text '" + i + "' is available.")
+                except NoSuchElementException as err:
+                    print("Exception: " + err.msg)
+            
+            case "PARTIAL_LINK_TEXT":
+                try:
+                    e = driver.find_element(By.PARTIAL_LINK_TEXT, i)
+                    print("The element with partial link text '" + i + "' is available.")
+                except NoSuchElementException as err:
+                    print("Exception: " + err.msg)
+            
+            case "CSS_SELECTOR":
+                try:
+                    e = driver.find_element(By.CSS_SELECTOR, i)
+                    print("The element with CSS selector '" + i + "' is available.")
+                except NoSuchElementException as err:
+                    print("Exception: " + err.msg)
+            
+            case "XPATH":
+                try:
+                    e = driver.find_element(By.XPATH, i)
+                    print("The element with XPath '" + i + "' is available.")
+                except NoSuchElementException as err:
+                    print("Exception: " + err.msg)
+            
+            case _:
+                print("Invalid element.")
+
+   
+
+    # Rerun in dock for 'x' times
     def rerun(self, x, y):
         for _ in range(x):
             y()
--- test_script.py
+++ test_script.py
@@ -7,8 +7,31 @@ d.driver.get(getUrl)
 # Run Function
 def run():
     # Your script goes here
-    print("Hello world!")
+    print("Test ran successfully!")
 
 
-i = d.Driver(d.driver)
-i.rerun(3, lambda: run())
+
+
+# New instance of a driver
+test = d.Driver(d.driver)
+
+
+
+### Use Cases Examples ###
+
+# Rerunnning a test for multiple times
+# test.rerun(3, lambda: run())
+
+
+# Gets the title of the webpage
+# test.getTitle()
+
+
+# Check for the availability of the element in the webpage
+# test.checkElement("CLASS_NAME","form-control")
+
+
+# Check for if the button is clickable or not
+# This check operation uses the url field change while submit, so make sure to fill up the desired input field before submission.
+# test.checkClick("button")
+
